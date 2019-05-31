44faeaeccd9bad0b71b8194d8f7a013b13c99ed8
Siddharth Singh <sidharth_0903@hotmail.com>
2019-05-31 11:52:10 +0530

    Added timit decorator

--- useful_utilities.py
+++ useful_utilities.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 
+
 def debug_func(func):
     from pprint import pprint
-    def wrapper(*args,**kwargs):
+
+    def wrapper(*args, **kwargs):
         print("ARGS:")
         pprint(args)
         print("KWARGS:")
         pprint(kwargs)
         print("DONE: {}".format(func.__name__))
-        res = func(*x)
+        res = func(*args, **kwargs)
         print("RESULT: {}".format(res.__str__()))
         return res
+
+    return wrapper
+
+
+def timeit(func):
+    import time
+
+    def wrapper(*args, **kwargs):
+        print("Executing {}...".format(func.__name__))
+        start_time = time.time()
+        res = func(*args, **kwargs)
+        end_time = time.time()
+        print("Executing time:")
+        print(end_time - start_time)
+        return res
+
     return wrapper
