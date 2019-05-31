e40ad16dfc234352f6ed4942f6317e2b4b904bc3
Ayush Agrawal <ayushagrawal1801@gmail.com>
2019-05-31 20:47:41 +0530

    Markdown page : image
    usage of name,content etc.

---
+++ markdown.py
@@ -0,0 +1,27 @@
+from useful_utilities import *
+
+@debug_func
+def markdown(title,content,image,license,source,authors,collection,location):
+	template = """# {}
+### {}
+![alt text]({})
+##### *License*: {}
+##### *Source*: {}
+{}"""
+	footnote = ""
+	if len(authors)!=0:
+		footnote += "##### Authors: "
+		for author in authors[:-1]:
+			footnote += "{}, ".format(author)
+		footnote += "{}\n".format(authors[-1])
+	if len(collection)!=0:
+		footnote += "##### Collection: "
+		for collec in collection[:-1]:
+			footnote += "{}, ".format(collec)
+		footnote += "{}\n".format(collection[-1]) 
+	if len(location)!=0:
+		footnote += "##### Location: "
+		for loc in location[:-1]:
+			footnote += "{}, ".format(loc)
+		footnote += "{}".format(location[-1])
+	return template.format(title, content, image, license, source, footnote)
