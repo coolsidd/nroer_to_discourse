53e5f92dcf1076ea099638c8d171ffe16de6d40e
Siddharth Singh <sidharth_0903@hotmail.com>
2019-06-03 17:33:21 +0530

    Added NROER Markdown template for video media

--- markdown.py
+++ markdown.py
@@ -1,8 +1,43 @@
 from useful_utilities import *
 
-@debug_func
-def markdown(title,content,image,license,source,authors,collection,location):
-	template = """# {}
+
+def markdown_video(
+    title,
+    content,
+    thumbnail,
+    video_link,
+    license,
+    source,
+    authors,
+    collection,
+    location,
+):
+    template = """# {}
+### {}
+<a href="{}" target="_blank"><img src="{}" alt="Video" width="240" height="180" border="10" /></a>
+##### *License*: {}
+##### *Source*: {}
+{}"""
+    footnote = ""
+    if len(authors) != 0:
+        footnote += "##### Authors: "
+        for author in authors[:-1]:
+            footnote += "{}, ".format(author)
+        footnote += "{}\n".format(authors[-1])
+    if len(collection) != 0:
+        footnote += "##### Collection: "
+        for collec in collection[:-1]:
+            footnote += "{}, ".format(collec)
+        footnote += "{}\n".format(collection[-1])
+    if len(location) != 0:
+        footnote += "##### Location: "
+        for loc in location[:-1]:
+            footnote += "{}, ".format(loc)
+        footnote += "{}".format(location[-1])
+    return template.format(
+        title, content, video_link, thumbnail, license, source, footnote
+    )
+
 
 def markdown_image(
     title, content, image, license, source, authors, collection, location
@@ -13,20 +48,20 @@ def markdown_image(
 ##### *License*: {}
 ##### *Source*: {}
 {}"""
-	footnote = ""
-	if len(authors)!=0:
-		footnote += "##### Authors: "
-		for author in authors[:-1]:
-			footnote += "{}, ".format(author)
-		footnote += "{}\n".format(authors[-1])
-	if len(collection)!=0:
-		footnote += "##### Collection: "
-		for collec in collection[:-1]:
-			footnote += "{}, ".format(collec)
-		footnote += "{}\n".format(collection[-1]) 
-	if len(location)!=0:
-		footnote += "##### Location: "
-		for loc in location[:-1]:
-			footnote += "{}, ".format(loc)
-		footnote += "{}".format(location[-1])
-	return template.format(title, content, image, license, source, footnote)
+    footnote = ""
+    if len(authors) != 0:
+        footnote += "##### Authors: "
+        for author in authors[:-1]:
+            footnote += "{}, ".format(author)
+        footnote += "{}\n".format(authors[-1])
+    if len(collection) != 0:
+        footnote += "##### Collection: "
+        for collec in collection[:-1]:
+            footnote += "{}, ".format(collec)
+        footnote += "{}\n".format(collection[-1])
+    if len(location) != 0:
+        footnote += "##### Location: "
+        for loc in location[:-1]:
+            footnote += "{}, ".format(loc)
+        footnote += "{}".format(location[-1])
+    return template.format(title, content, image, license, source, footnote)
