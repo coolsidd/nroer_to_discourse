bae594b2e7de670a0efcfc7f30e31d5def37af80
Siddharth Singh <sidharth_0903@hotmail.com>
2019-06-09 13:54:03 +0530

    Fixed a minor bug caused due to wrapper class

--- interface_discourse.py
+++ interface_discourse.py
@@ -70,8 +70,8 @@ class discourse_interface:
     #     _request = _requst_copy
     #     TEST_MODE = False
 
-    @staticmethod
     def _request(
+        self,
         _type,
         url,
         json,
@@ -83,7 +83,7 @@ class discourse_interface:
         allow_redirects,
         **kwargs
     ):
-        for i in range(RETRIES):
+        for i in range(self.RETRIES):
             res = requests.request(
                 _type,
                 url,
@@ -98,7 +98,7 @@ class discourse_interface:
             )
             if res.status_code == 429:
                 time = json.loads(res.content)["extras"]["wait_seconds"]
-                if not QUIET_MODE:
+                if not self.QUIET_MODE:
                     pprint(res.content)
                     print("Waiting for throttle...")
                     print("Sleeping for {} seconds".format(time))
@@ -106,10 +106,9 @@ class discourse_interface:
             else:
                 return res
 
-    @staticmethod
-    def parse_response(response):
+    def parse_response(self, response):
         LAST_RES = response
-        if QUIET_MODE:
+        if self.QUIET_MODE:
             return response
         try:
             pprint(json.loads(response.content))
@@ -140,7 +139,7 @@ class discourse_interface:
         params.setdefault("api_key", self.API_KEY)
         params.setdefault("api_username", self.API_USERNAME)
         headers = {"Accept": "application/json; charset=utf-8"}
-        response = _request(
+        response = self._request(
             "GET",
             url,
             json=json,
@@ -152,7 +151,7 @@ class discourse_interface:
             allow_redirects=allow_redirects,
             **kwargs
         )
-        return parse_response(response)
+        return self.parse_response(response)
 
     def put_request(
         self,
@@ -168,7 +167,7 @@ class discourse_interface:
         params.setdefault("api_key", self.API_KEY)
         params.setdefault("api_username", self.API_USERNAME)
         headers = {"Accept": "application/json; charset=utf-8"}
-        response = _request(
+        response = self._request(
             "PUT",
             url,
             json=json,
@@ -180,7 +179,7 @@ class discourse_interface:
             allow_redirects=allow_redirects,
             **kwargs
         )
-        return parse_response(response)
+        return self.parse_response(response)
 
     def post_request(
         self,
@@ -202,7 +201,7 @@ class discourse_interface:
         #     "Api-Key": API_KEY,
         #     "Api-Username": API_USERNAME
         # }
-        response = _request(
+        response = self._request(
             "POST",
             url,
             json=json,
@@ -214,19 +213,19 @@ class discourse_interface:
             allow_redirects=allow_redirects,
             **kwargs
         )
-        return parse_response(response)
+        return self.parse_response(response)
 
     @testable
     def get_categories(self,):
         end_point = "/categories.json"
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = dict()
         return self.get_request(data, url_with_end_point)
 
     @testable
     def create_category(self, name, color, text_color):
         end_point = "/categories.json"
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = {"name": name, "color": color, "text_color": text_color}
         return self.post_request(data, url_with_end_point)
 
@@ -242,7 +241,7 @@ class discourse_interface:
         user_fields="",
     ):
         end_point = "/users"
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = locals()
         return self.post_request(data, url_with_end_point)
 
@@ -257,7 +256,7 @@ class discourse_interface:
             "composer",
         }
         end_point = "/uploads.json"
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         synchronous = str(synchronous).lower()
         data = {"type": _type, "userid": userid, "synchronous": synchronous}
 
@@ -267,21 +266,21 @@ class discourse_interface:
     @testable
     def get_tag(self, tag):
         end_point = "/tags/{}".format(tag)
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = {}
         return self.get_request(data, url_with_end_point)
 
     @testable
     def get_tag_groups(self,):
         end_point = "/tag_groups.json"
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = locals()
         return self.get_request(data, url_with_end_point)
 
     @testable
     def create_new_tag_group(self, name, tag_names):
         end_point = "/tag_groups.json"
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = locals()
         return self.post_request(data, url_with_end_point)
 
@@ -297,7 +296,7 @@ class discourse_interface:
         created_at=None,
     ):
         end_point = "/posts.json"
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = locals()
         data = {k: v for k, v in data.items() if v is not None}
         return self.post_request(data, url_with_end_point)
@@ -305,28 +304,28 @@ class discourse_interface:
     @testable
     def set_tags_to_topic(self, topic_name, topic_id, tags):
         end_point = "/t/{}/{}.json".format(topic_name, topic_id)
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = {"tags[]": tags}
         return self.put_request(data, url_with_end_point)
 
     @testable
     def update_a_tag_group(self, group_id, group_name, tags):
         end_point = "/tag_groups/{}.json".format(group_id)
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = {"name": group_name, "tag_names[]": tags}
         return self.put_request(data, url_with_end_point)
 
     @testable
     def close_topic(self, topic_name, topic_id):
         end_point = "/t/{}/{}/status.json".format(topic_name, topic_id)
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = {"status": "closed", "enabled": "true"}
         return self.put_request(data, url_with_end_point)
 
     @testable
     def open_topic(self, topic_name, topic_id):
         end_point = "/t/{}/{}/status.json".format(topic_name, topic_id)
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = {"status": "closed", "enabled": "false"}
         return self.put_request(data, url_with_end_point)
 
@@ -335,13 +334,13 @@ class discourse_interface:
         end_point = "/t/{}/{}/status.json".format(topic_name, topic_id)
         if datetime_obj is None:
             datetime_obj = datetime.datetime(year=3019, month=12, day=31)
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = {"status": "pinned", "enabled": "true", "until": datetime_obj}
         return self.put_request(data, url_with_end_point)
 
     @testable
     def unpin_topic(self, topic_name, topic_id):
         end_point = "/t/{}/{}/status.json".format(topic_name, topic_id)
-        url_with_end_point = urljoin(URL, end_point)
+        url_with_end_point = urljoin(self.URL, end_point)
         data = {"status": "pinned", "enabled": "false"}
         return self.put_request(data, url_with_end_point)
