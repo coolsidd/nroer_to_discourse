04c30aebbee514f5b6e80b13ade97070451fed02
Ayush Agrawal <ayushagrawal1801@gmail.com>
2019-06-13 22:27:38 +0530

    Identify and Store FUnctions are written in SQL.

--- sql_db_funcs.py
+++ sql_db_funcs.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 def identify(name_entered, value_entered, filename):
 	conn = sqlite3.connect(filename)
 	c = conn.cursor()
@@ -14,4 +15,21 @@ def identify(name_entered, value_entered, filename):
 	else:
 		return None
 
-           
+def store(table_name, value_entered, data_entered, db_name):
+        table_name = str(table_name)
+        value_entered = str(value_entered)
+
+        connect_me = sqlite3.connect(db_name)
+        cur = connect_me.cursor()
+        data_check = (cur.execute("SELECT COUNT(*) FROM {} WHERE value=?", .format(table_name), (value_entered))).fetchone()[0]
+
+        if data_check != 0:
+                cur.execute("UPDATE {} SET data=data+{} WHERE value=?", .format(table_name,data_entered), (value_entered))
+
+        else:
+                cur.execute("INSERT INTO {} VALUES(?,?)", .format(table_name), (value_entered, data_entered))
+
+        connect_me.commit()
+        connect_me.close()
+
+
